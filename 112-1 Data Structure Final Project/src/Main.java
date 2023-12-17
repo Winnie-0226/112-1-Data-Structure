905a2287e6877c36bd774f5f6f03e25cad85573c
Winnie-0226 <111306081@g.nccu.edu.tw>
2023-12-17 18:47:42 +0800

    stage0

--- 112-1 Data Structure Final Project/src/Main.java
+++ 112-1 Data Structure Final Project/src/Main.java
@@ -1,11 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    int a = 5;
+    public static void main(String[] args) throws IOException {
+        System.out.println("Please type URL:");
+        Scanner sc = new Scanner(System.in);
 
-    public int getA() {
-        return a;
-    }
+        // 關鍵字
+        String keyword = "的";
 
-    public void setA(int a) {
-        this.a = a;
+        while (sc.hasNext()) {
+            String urlStr = sc.next();
+
+            WordCounter counter = new WordCounter(urlStr);
+            System.out.println(keyword + " appears " + counter.countKeyword(keyword) + " times.");
+        }
+        sc.close();
     }
 }
+
+//http://big5.news.cn/gate/big5/www.news.cn/2023-12/01/c_1212308903.htm
